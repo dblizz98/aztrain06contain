13da2d189eb41e72c45523c0762af2a50aa234a9
David Blizzard <david.blizzard@i2ipophealth.com>
2019-06-19 16:04:14 -0500

    Add project files.

---
+++ aztrain06contain/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aztrain06contain.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
